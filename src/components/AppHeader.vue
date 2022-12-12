def5f1682a64fafcca311be0cca17401df997791
tsukigahara <satsuki.tksm@gmail.com>
2022-12-12 18:18:28 +0100

    added header layout

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,86 @@
+<script>
+import 'bootstrap';
+export default {
+
+}
+</script>
+
+<template>
+
+    <header>
+        <a href="">
+            <img src="../assets/dc-logo.png" alt="">
+        </a>
+        <ul>
+            <li>
+                <a href="">lorem</a>
+            </li>
+            <li>
+                <a href="">lorem</a>
+            </li>
+            <li>
+                <a href="">lorem</a>
+            </li>
+            <li>
+                <a href="">lorem</a>
+            </li>
+            <li>
+                <a href="">lorem</a>
+            </li>
+            <li>
+                <a href="">lorem</a>
+            </li>
+            <li>
+                <a href="">lorem</a>
+            </li>
+            <li>
+                <a href="">lorem</a>
+            </li>
+            <li>
+                <a href="">lorem</a>
+            </li>
+            <li>
+                <a href="">lorem</a>
+            </li>
+        </ul>
+
+    </header>
+
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/partials/mixins' as *;
+@use '../styles/partials/variables' as *;
+
+header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+    min-height: 100px;
+    background-color: beige;
+}
+
+a>img {
+    width: 80px;
+    height: auto;
+}
+
+ul {
+    height: 100%;
+    margin: 0 20px;
+    list-style: none;
+    display: flex;
+    gap: 0 30px;
+
+    li {
+        height: 100%;
+        display: inline-block;
+
+        a {
+            text-decoration: none;
+        }
+
+    }
+}
+</style>
